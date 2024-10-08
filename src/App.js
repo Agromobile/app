988cf49730d2e5b68c120273fcd3ada49df3f573
PC-Ngumoha <pcngumoha@gmail.com>
2024-10-08 19:02:44 +0100

    Update: fix wrong Home importation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components';
-import { Home } from './routes';
+import { Home } from './pages';
 
 function App() {
   return (
