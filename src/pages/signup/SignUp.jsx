d33a94dc6a2f0ec562d94e349f4a09eac8061458
PC-Ngumoha <pcngumoha@gmail.com>
2024-10-11 10:51:31 +0100

    work on input fields for text, password & email

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,35 +1,107 @@
 import './signup.scss';
 import { useState } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import logo from '../../assets/small-logo.png';
 
+// Utility Hook: used to handle active state for inputs
+function useActiveInput() {
+  const [active, setActive] = useState(false);
+
+  const handleToggleActiveLabel = (evt) => {
+    if (evt.target.value === '') {
+      setActive(false);
+    } else {
+      setActive(true);
+    }
+  };
+
+  return { active, handleToggleActiveLabel };
+}
+
+// TextInput: Utility component for handling text input
+function TextInput() {
+  const { active, handleToggleActiveLabel } = useActiveInput();
+  return (
+    <div className={`input ${active ? 'active' : ''}`}>
+      <input
+        type="text"
+        id="text"
+        className="stretch"
+        onChange={handleToggleActiveLabel}
+      />
+      <label htmlFor="text">Label</label>
+    </div>
+  );
+}
+
+// PasswordInput: Utility component for handling password input
+function PasswordInput() {
+  const [isVisible, setIsVisible] = useState(false);
+  const { active, handleToggleActiveLabel } = useActiveInput();
+
+  const handleToggleVisibility = (evt) => {
+    evt.preventDefault();
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className={`input ${active ? 'active' : ''}`}>
+      <input
+        type={`${isVisible ? 'text' : 'password'}`}
+        id="password"
+        onChange={handleToggleActiveLabel}
+      />
+      <label htmlFor="password">Label</label>
+      <button
+        className="toggle-visibility"
+        onClick={handleToggleVisibility}
+      >
+        {isVisible ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+      </button>
+    </div>
+  );
+}
+
+// TODO: EmailInput: Utility component for handling email input along with verification
+function EmailInput() {
+  const { active, handleToggleActiveLabel } = useActiveInput();
+  return (
+    <div className={`input ${active ? 'active' : ''}`}>
+      <input
+        type="email"
+        id="email"
+        className="stretch"
+        onChange={handleToggleActiveLabel}
+        autoCorrect="email"
+      />
+      <label htmlFor="email">Label</label>
+    </div>
+  );
+}
+
+// TODO: PhoneInput: Utility component for handling phone input along with verification
+
+// TODO: SignUpPersonal: Utility component - will contain the personal signup form when I'm done.
 function SignUpPersonal() {
   return (
     <form>
-      <div>
-        <input
-          type="text"
-          id="email"
-          placeholder="Enter personal email address"
-        />
-      </div>
+      <TextInput />
+      <PasswordInput />
+      <EmailInput />
     </form>
   );
 }
 
+// TODO: SignUpBusiness: Utility component - will contain the business signup form when I'm done
 function SignUpBusiness() {
   return (
     <form>
-      <div>
-        <input
-          type="text"
-          id="email"
-          placeholder="Enter business email address"
-        />
-      </div>
+      <TextInput />
     </form>
   );
 }
 
+// SignUp: Base SignUp form - contains the things shared by personal & business signup forms
 export default function SignUp() {
   const [activeForm, setActiveForm] = useState('personal');
 
